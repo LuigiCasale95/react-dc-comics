a5f72aaa0586a61bfae647524694d5e8746830c2
LuigiCasale95 <luigicasale95@gmail.com>
2025-09-24 18:56:10 +0200

    creazione liste del footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,6 +1,61 @@
 const MyFooter = () => {
     return (
-        <div>FOOTER</div>
+        <footer>
+            <div className="containerFooter">
+
+            <div className="listFooter">
+                <section>
+                    <ul>
+                        <li>DC COMICS</li>
+                        <li><a href="#">Charecters</a></li>
+                        <li><a href="#">Comics</a></li>
+                        <li><a href="#">Movies</a></li>
+                        <li><a href="#">TV</a></li>
+                        <li><a href="#">Games</a></li>
+                        <li><a href="#">Videos</a></li>
+                        <li><a href="#">News</a></li>
+                    </ul>
+                    <ul>
+                        <li>SHOP</li>
+                        <li><a href="#">Shop DC</a></li>
+                        <li><a href="#">Shop DC collectibles</a></li>
+                    </ul>
+                </section>
+                <section>
+                    <ul>
+                        <li>DC</li>
+                        <li><a href="#">Terms of use</a></li>
+                        <li><a href="#">Privacy policy</a></li>
+                        <li><a href="#">Ad Choices</a></li>
+                        <li><a href="#">Advertising</a></li>
+                        <li><a href="#">Jobs</a></li>
+                        <li><a href="#">Subscription</a></li>
+                        <li><a href="#">Talent Worksshoop</a></li>
+                        <li><a href="#">CPSC Certificates</a></li>
+                        <li><a href="#">Ratingss</a></li>
+                        <li><a href="#">Shop Help</a></li>
+                        <li><a href="#">Contact us</a></li>
+                    </ul>
+                </section>
+                <section>
+                    <ul>
+                        <li>SITES</li>
+                        <li><a href="#">DC</a></li>
+                        <li><a href="#">MAD Magazine</a></li>
+                        <li><a href="#">DC Kids</a></li>
+                        <li><a href="#">DC Universe</a></li>
+                        <li><a href="#">DC Power Visa</a></li>
+
+                    </ul>
+                </section>
+            </div>
+
+            <div className="imgFooter">
+
+            </div>
+
+            </div>
+        </footer>
     )
 }
 export default MyFooter;
--- src/components/main.jsx
+++ src/components/main.jsx
@@ -11,11 +11,11 @@ const MyMain = () => {
             <div className="blueBar">
                 <div className="containerMain">
                     <ul className="listMain">
-                        <li><a href="@"> <img src="./img/buy-comics-digital-comics.png" alt="Logo digital" /> <span>DIGITAL COMICS</span> </a></li>
-                        <li><a href="@"> <img src="./img/buy-comics-merchandise.png" alt="Logo T-shirt" /> <span> DC MERCCHANDISE</span> </a></li>
-                        <li><a href="@"> <img src="./img/buy-comics-subscriptions.png" alt="Logo card" /> <span>SUBSCRIPTION</span> </a></li>
-                        <li><a href="@"> <img src="./img/buy-comics-shop-locator.png" alt="Logo posizione" /> <span>COMICS SHOP LOCATOR</span> </a></li>
-                        <li><a href="@"> <img src="./img/buy-dc-power-visa.svg" alt="Logo power Visa" /> <span> DC POWER VISA</span> </a></li>
+                        <li><a href="@"> <img className="iconDC" src="./img/buy-comics-digital-comics.png" alt="Logo digital" /> <span>DIGITAL COMICS</span> </a></li>
+                        <li><a href="@"> <img className="iconDC" src="./img/buy-comics-merchandise.png" alt="Logo T-shirt" /> <span> DC MERCCHANDISE</span> </a></li>
+                        <li><a href="@"> <img className="iconDC" src="./img/buy-comics-subscriptions.png" alt="Logo card" /> <span>SUBSCRIPTION</span> </a></li>
+                        <li><a href="@"> <img className="iconDcLocator" src="./img/buy-comics-shop-locator.png" alt="Logo posizione" /> <span>COMICS SHOP LOCATOR</span> </a></li>
+                        <li><a href="@"> <img className="iconDC" src="./img/buy-dc-power-visa.svg" alt="Logo power Visa" /> <span> DC POWER VISA</span> </a></li>
                     </ul>
                 </div>
 
